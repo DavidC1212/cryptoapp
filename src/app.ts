80dac64c7f7475d879d261a17aee94ec01440bef
DavidC1212 <sus8521@gmail.com>
2024-03-05 11:34:41 +0200

    add dashboard

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import  express from "express";
+import userRouter from './routers/users';
+import path from 'path';
+
+const server = express()
+const PORT = 8080;
+
+server.set('views', path.resolve(__dirname,  'views'))
+server.set('view engine', 'ejs')
+server.use('/users', userRouter); 
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
